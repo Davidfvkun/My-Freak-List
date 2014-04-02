e653fc7a1b9636dc2e8aa53dcfaee209fe3a8eb6
David Femenia Vazquez <divad19_09_94@hotmail.com>
2014-04-02 18:38:16 +0200

    Paginacion en busqueda material, slider en el perfil de usuario con los favoritos

--- config.php
+++ config.php
@@ -3,5 +3,8 @@
                     ORM::configure('mysql:host=localhost;dbname=myfreakzone; charset=utf8');
                     ORM::configure('username','root');
                     ORM::configure('password','');
-                    /* La BD se llama myfreakzone y el usuario de MySQL es root sin contraseña (En las pruebas que
-                    	estoy realizando mientras se hace la aplicacion )*/
+                    
+                    $GLOBALS['generos'] = array('No filtrar',
+                        'lucha','accion','misterio',
+                        'apocaliptico','shonen','shojo',
+                        'drama','amor','infantil');
