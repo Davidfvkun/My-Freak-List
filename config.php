d7e2d982ee4799f4976a8f0941cc7425ec5e8ee2
David Femenia Vazquez <divad19_09_94@hotmail.com>
2014-04-07 20:47:57 +0200

    Añadida Licencia

--- config.php
+++ config.php
@@ -1,5 +1,34 @@
                             
-                    <?php
+                    <?php/*  My Freak Zone - My Freak List - Web application for films, series and animes (Japanase Animation)
+						  Copyright (C) 2014: David Femenía Vázquez
+
+						  This program is free software: you can redistribute it and/or modify
+						  it under the terms of the GNU Affero General Public License as published by
+						  the Free Software Foundation, either version 3 of the License, or
+						  (at your option) any later version.
+
+						  This program is distributed in the hope that it will be useful,
+						  but WITHOUT ANY WARRANTY; without even the implied warranty of
+						  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+						  GNU Affero General Public License for more details.
+
+						  You should have received a copy of the GNU Affero General Public License
+						  along with this program.  If not, see [http://www.gnu.org/licenses/]. */
+						                    /*  My Freak Zone - My Freak List - Web application for films, series and animes (Japanase Animation)
+						  Copyright (C) 2014: David Femenía Vázquez
+
+						  This program is free software: you can redistribute it and/or modify
+						  it under the terms of the GNU Affero General Public License as published by
+						  the Free Software Foundation, either version 3 of the License, or
+						  (at your option) any later version.
+
+						  This program is distributed in the hope that it will be useful,
+						  but WITHOUT ANY WARRANTY; without even the implied warranty of
+						  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+						  GNU Affero General Public License for more details.
+
+						  You should have received a copy of the GNU Affero General Public License
+						  along with this program.  If not, see [http://www.gnu.org/licenses/]. */
                     ORM::configure('mysql:host=localhost;dbname=myfreakzone; charset=utf8');
                     ORM::configure('username','root');
                     ORM::configure('password','');
